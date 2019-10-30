a927680f437c821647db88952eeefa40cd032f5e
Vivek <viv1g3pta@gmail.com>
2019-10-31 00:19:28 +0530

    add main js script on component load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  constructor(
+    private renderer2: Renderer2,
+    @Inject( DOCUMENT ) private document
+  ) {
+  }
+  ngOnInit() {
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = './assets/js/main.js';
+    s.text = ``;
+    this.renderer2.appendChild(this.document.body, s);
+  }
+
 }
